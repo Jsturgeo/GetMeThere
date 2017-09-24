ac6d13289fff8ec57837510a9396c71315026dfb
Denis Zubo <denis@zubo.ru>
2017-09-24 10:14:45 -0700

    print only first cheapest/fastest

--- apis/ranking.py
+++ apis/ranking.py
@@ -7,10 +7,14 @@ end_coords =  [49.274196, -123.15113] # Kits Beach
 all_routes = get_modo_routes(start_coords, end_coords) + get_mobi_routes(start_coords, end_coords)
 
 def cheapest_trip(routes):
-	return sorted(routes, key=lambda r: (r.cost, r.duration))[0]
+	return sorted(routes, key=lambda r: (r.cost, r.duration))
 
 def shortest_time(routes):
-	return sorted(routes, key=lambda r: (r.duration, r.cost))[0]
+	# return sorted(routes, key=lambda r: (r.duration['value'], r.cost))[0]
+	# return sorted(routes, key=lambda r: (r.duration))
+	return sorted(routes, key=lambda r: (r.duration if not (r.duration is None) else 1e9))
 
-print cheapest_trip(all_routes)
-print shortest_time(all_routes)
+print "Cheapest one:"
+print cheapest_trip(all_routes)[0]
+print "Shortest one:"
+print shortest_time(all_routes)[0]
