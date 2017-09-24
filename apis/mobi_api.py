a1ad93937afb30e9b9db0edd0706b27e6c97c93a
Jsturgeo <juliesturgeon@gmail.com>
2017-09-23 18:27:30 -0700

    adding cost functions (need testing) & changing get_routes function

--- apis/mobi_api.py
+++ apis/mobi_api.py
@@ -5,34 +5,55 @@ from itertools import product
 
 mobi_url = 'https://mountainmath.ca/mobi/stations'
 
-current_location = [-123.114867, 49.283481] # BCIT Downtown
-destination_coords =  [-123.15113, 49.274196] # Kits Beach
+start_coords = [-123.114867, 49.283481] # BCIT Downtown
+end_coords =  [-123.15113, 49.274196] # Kits Beach
 
 def get_mobi_stations_data():
 	response = requests.get(mobi_url)
 	resp_parsed = eval(response.text.replace('true', 'True').replace('false', 'False').replace('null', 'None'))
 	return resp_parsed['features']
 
-def get_closest_stations(current_location, stations_data, top_n=3):
+def get_closest_stations(start_coords, stations_data, top_n=3):
 	bikes_data = []
 	for station in stations_data:
 		if station['properties']['avl_bikes'] == 0 or not station['properties']['operative']:
 			continue
-		dist_from_current = haversine(station['geometry']['coordinates'], current_location)
+		dist_from_current = haversine(station['geometry']['coordinates'], current_coords)
 		bikes_data.append((dist_from_current, station['properties']['name'], station['geometry']['coordinates']))
 	sorted_bikes = sorted(bikes_data, key=lambda x: x[0])
 	return sorted_bikes[:top_n]
 
-def potential_routes(current_location, closest_stations_start, closest_stations_end, destination_coords, method='bicycling'):
-	first_legs = [{'start_coord' : current_location, 'end_coord' : station[2], 'mode' : 'walking'} for station in closest_stations_start]
-	last_legs = [{'start_coord' : station[2], 'end_coord' : destination_coords, 'mode' : 'walking'} for station in closest_stations_end]
+def potential_routes(start_coords, closest_stations_start, closest_stations_end, end_coords, method='bicycling'):
+	first_legs = [{'start_coords' : current_coords, 'end_coords' : station[2], 'mode' : 'walking'} for station in closest_stations_start]
+	last_legs = [{'start_coords' : station[2], 'end_coords' : end_coords, 'mode' : 'walking'} for station in closest_stations_end]
 	first_and_last = list(product(first_legs, last_legs))
-	full_routes = [(fl[0], {'start_coord' : fl[0]['end_coord'], 'end_coord' : fl[1]['start_coord'], 'mode' : method}, fl[1]) for fl in first_and_last]
+	full_routes = [(fl[0], {'start_coords' : fl[0]['end_coords'], 'end_coords' : fl[1]['start_coords'], 'mode' : method}, fl[1]) for fl in first_and_last]
 	return full_routes
 
-def get_trips_routes():
+def get_trips_routes(start_coords, end_coords):
 	stations_data = get_mobi_stations_data()
-	closest_stations_start = get_closest_stations(current_location, stations_data)
-	closest_stations_end = get_closest_stations(destination_coords, stations_data)
-	routes = potential_routes(current_location, closest_stations_start, closest_stations_end, destination_coords)
+	closest_stations_start = get_closest_stations(start_coords, stations_data)
+	closest_stations_end = get_closest_stations(end_coords, stations_data)
+	routes = potential_routes(start_coords, closest_stations_start, closest_stations_end, end_coords)
 	return routes
+
+def get_route_cost(route, membership_type):
+	total_biking_time = sum(l['time'] for l in route if l['mode'] == 'biking')
+	if total_biking_time <= 30:
+		return 0
+	overage_time = total_biking_time - 30
+	if membership_type == '24_hour_pass':
+		return ceil(float(overage_time) / 30) * 5
+	elif membership_type in ('90_day_pass', '365_day_pass_standard'):
+		overage_cost = 2
+		if overage_time > 30:
+			overage_time += -30
+			overage_cost += ceil(float(overage_time) / 30) * 3
+		return overage_cost
+	elif membership_type == '365_day_pass_plus':
+		if overage_time <= 30:
+			return 0
+		overage_time += -30
+		return ceil(float(overage_time) / 30) * 3
+	else: #unknown membership type
+		return 0
--- apis/modo_api.py
+++ apis/modo_api.py
@@ -4,18 +4,20 @@ import pprint
 import time
 from haversine import haversine
 
-current_location = [-123.114867, 49.283481] # BCIT Downtown
-destination_coords =  [-123.15113, 49.274196] # Kits Beach
+start_coords = [-123.114867, 49.283481] # BCIT Downtown
+end_coords =  [-123.15113, 49.274196] # Kits Beach
 
-availability_url = 'https://bookit.modo.coop/api/availability'
+availability_url = 'https://bookit.modo.coop/api/availability/'
+cost_url = 'https://bookit.modo.coop/api/cost/'
 
-def parse_mobi_data(raw_data):
+def parse_mobi_data(raw_data, start_time):
 	mobi_cars = []
 	for key in raw_data.keys():
 		car = raw_data[key]
-		if car['Duration'] == 'throttled':
+		if car['Duration'] == 'throttled' or car['Start Time'] != start_time:
 			continue
-		mobi_cars.append((car['CarID'], [float(car['Longitude']), float(car['Latitude'])], car['Duration'], car['CarDescription'], car['LocationName']))
+		mobi_cars.append((car['CarID'], [float(car['Longitude']), float(car['Latitude'])], 
+			{'description' : car['CarDescription'], 'location_name' : car['LocationName'], 'start_time' : car['Start Time'], 'end_time' : car['EndTime']}))
 	return mobi_cars
 
 def get_closest_cars(current_location, mobi_data, top_n=3):
@@ -26,22 +28,36 @@ def get_closest_cars(current_location, mobi_data, top_n=3):
 	sorted_cars = sorted(cars_data, key=lambda x: x[0])
 	return sorted_cars[:top_n]
 
-def potential_routes(current_location, closest_cars, destination_coords, method='driving'):
-	first_legs = [{'start_coord' : current_location, 'end_coord' : car[1][1], 'mode' : 'walking'} for car in closest_cars]
-	last_legs = [{'start_coord' : car[1][1], 'end_coord' : destination_coords, 'mode' : method} for car in closest_cars]
+def potential_routes(start_coords, closest_cars, end_coords, method='driving'):
+	first_legs = [{'start_coords' : current_location, 'end_coords' : car[1][1], 'mode' : 'walking'} for car in closest_cars]
+	last_legs = [{'start_coords' : car[1][1], 'end_coords' : end_coords, 'mode' : method, 'meta' : car[2]} for car in closest_cars]
 	full_routes = zip(first_legs, last_legs)
 	return full_routes
 
 def get_mobi_data():
 	now = int(time.time())
-	response = requests.get(availability_url + '/' + str(now))
+	response = requests.get(availability_url + str(now))
 	resp_json = json.loads(response.text)
 	raw_data = resp_json['Data']
-	return raw_data
+	start_time = resp_json['Request']['RequestStart']
+	return raw_data, start_time
 
-def get_trips_routes():
-	raw_data = get_mobi_data()
-	parsed_data = parse_mobi_data(raw_data)
+def get_trips_routes(start_coords, end_coords):
+	raw_data, start_time = get_mobi_data()
+	parsed_data = parse_mobi_data(raw_data, start_time)
 	closest_cars = get_closest_cars(current_location, parsed_data)
-	routes = potential_routes(current_location, closest_cars, destination_coords)
+	routes = potential_routes(start_coords, closest_cars, end_coords)
 	return routes
+
+def get_route_cost(route, membership_type):
+	'''
+	only supporting 'Roaming' and 'Business' memberships for now since I can't figure out
+	why this API hates spaces in the URL
+	'''
+	driving_distance = sum(l['distance'] for l in route if l['mode'] == 'driving')
+	driving_route = [l for l in route if l['mode'] == 'driving'][0]
+	https://bookit.modo.coop/api/cost/{plan_name}/{start_timestamp}/{end_timestamp}/{distance}
+	url = cost_url + membership_type + '/' + driving_route['meta']['start_time'] + '/' + driving_route['meta']['end_time'] + '/' + driving_route['distance']
+	response = requests.get(url)
+	resp_parsed = eval(response.text.replace('true', 'True').replace('false', 'False').replace('null', 'None'))
+	return float(resp_parsed['TotalCharge'])
